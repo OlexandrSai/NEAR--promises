08945b2dae6060123f8d8622166a21dd66d3d4b8
OlexandrSai <38455192+OlexandrSai@users.noreply.github.com>
2021-10-20 16:28:19 +0300

    fixes

--- src/components/FifthSection.vue
+++ src/components/FifthSection.vue
@@ -3,12 +3,12 @@
         <div class="w-full h-screen bg-gray-800" style="z-index: -2;">
             <div class="relative w-full h-full bg-text">
 
-                <img src="./img/pose-1.png" alt="" class="absolute top-0 left-0">
-                <img src="./img/pose-2.png" alt="" class="absolute top-0 right-0">
-                <img src="./img/pose-3.png" alt="" class="absolute bottom-0 left-0">
-                <img src="./img/pose-4.png" alt="" class="absolute bottom-0 right-0">
-                <img src="./img/hand_1.png" alt="" class="absolute top-64 left-80">
-                <img src="./img/hand_2.png" alt="" class="absolute top-96 right-80">
+                <img src="@/assets/img/pose-1.png" alt="" class="absolute top-0 left-0">
+                <img src="@/assets/img/pose-2.png" alt="" class="absolute top-0 right-0">
+                <img src="@/assets/img/pose-3.png" alt="" class="absolute bottom-0 left-0">
+                <img src="@/assets/img/pose-4.png" alt="" class="absolute bottom-0 right-0">
+                <img src="@/assets/img/hand_1.png" alt="" class="absolute top-64 left-80">
+                <img src="@/assets/img/hand_2.png" alt="" class="absolute top-96 right-80">
 
                 <div class="h-full flex flex-col items-center justify-center" style="z-index: 10;">
                     <h4 class="home-title text-center" style="line-height: 1.5;">Try NEAR Promises <br> <span class="bg-gradient-to-r from-blue-400 to-blue-500">right now</span> </h4>  <br>
