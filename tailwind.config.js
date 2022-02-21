5ad60fc86cda5f2637b2d7ce2249c7567c2ade06
kBastard <mihail.a.shevchuk@gmail.com>
2022-02-21 20:48:25 +0200

    NG | Init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,6 @@
 module.exports = {
-  purge: [`./index.html`, `./src/**/*.{vue,js,ts,jsx,tsx}`],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        gray: {
-          500: "#272739",
-          600: "#343346",
-          700: "#2D2C41",
-          800: "#313045",
-        },
-        blue: {
-          400: "#5DAAE2",
-          500: "#2A65FC",
-        },
-        orange: {
-          400: "#FF7A00",
-          500: "#DC2828",
-        }
-      }
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
